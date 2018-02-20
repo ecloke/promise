a4866dc573a2c57c663459b4b489cb34b3e1071b
Ethan Loke <euchern.loke@gmail.com>
2018-02-20 16:05:01 +0800

    add validation

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,5 +1,7 @@
 class Achievement < ApplicationRecord
+	validates :source, presence: true, uniqueness: { message: "has already been submitted" }
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
+
 end
