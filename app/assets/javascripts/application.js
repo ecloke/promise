f35856d4570272d84719d0a7d5ac0c0e1e625ff6
Ethan Loke <euchern.loke@gmail.com>
2018-02-23 10:13:26 +0800

    asd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require foundation
 //= require foundation
 //= require turbolinks
-//= require jquery
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
